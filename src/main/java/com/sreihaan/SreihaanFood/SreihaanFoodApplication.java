e20b101ac7bc392d226e5934e0ba17e264e1c385
Avinash <a4avinash@hotmail.com>
2020-12-28 21:54:13 +0530

    Adding initial files

---
+++ src/main/java/com/sreihaan/SreihaanFood/SreihaanFoodApplication.java
@@ -0,0 +1,13 @@
+package com.sreihaan.SreihaanFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreihaanFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreihaanFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreihaan/SreihaanFood/SreihaanFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreihaan.SreihaanFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreihaanFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
