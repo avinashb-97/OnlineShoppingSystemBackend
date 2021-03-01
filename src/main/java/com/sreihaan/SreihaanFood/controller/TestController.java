9c95dc253538b336592125592a1c9300400a2272
avinashb-97 <46625205+avinashb-97@users.noreply.github.com>
2021-03-01 14:27:54 +0530

    Fixed 404 error for api

--- src/main/java/com/sreihaan/SreihaanFood/controller/TestController.java
+++ src/main/java/com/sreihaan/SreihaanFood/controller/TestController.java
@@ -1,16 +1,11 @@
 package com.sreihaan.SreihaanFood.controller;
 
 import com.sreihaan.SreihaanFood.service.EmailSenderService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/test")
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
 
     @Autowired
